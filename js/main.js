30857defe09fc9eb6d7451b25ffb4aeac41c45f7
Neonila Shlikhtenko <shlikhtenkoneonila@gmail.com>
2015-02-04 22:12:13 +0200

    Add day and city name heading

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
+	function getDayByTimestamp(timestamp) {
+		return new Date(timestamp).toString().match(/^\w*/)[0];
+	}
+
 	$.get( "http://api.openweathermap.org/data/2.5/forecast/daily", {
 		q: "Kiev",
 		units: "metric",
 		cnt: 7
 	}, function(data) {
+		var $dayList = $("[data-js='day-list']");
+		$("[data-js='city']").text(data.city.name);
 		data.list.forEach(function(forecast){
-			$("<li></li>").text("City: " + data.city.name + ": min: " + forecast.temp.min + ", max: " + forecast.temp.max).appendTo($("ul"));
+			$("<li></li>").text(getDayByTimestamp(forecast.dt*1000) + " min: " + forecast.temp.min + ", max: " + forecast.temp.max).appendTo($dayList);
 		});
 	});
 });
