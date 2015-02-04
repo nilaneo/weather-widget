ae067e518c45a8b95b7481d2d31706d1946b8e5d
Neonila Shlikhtenko <shlikhtenkoneonila@gmail.com>
2015-02-04 23:09:04 +0200

    Use Mustache for client-side rendering

--- js/main.js
+++ js/main.js
@@ -1,17 +1,35 @@
 $(document).ready(function() {
-	function getDayByTimestamp(timestamp) {
+	function getDayByTimestamp (timestamp) {
 		return new Date(timestamp).toString().match(/^\w*/)[0];
 	}
 
-	$.get( "http://api.openweathermap.org/data/2.5/forecast/daily", {
+	function getTemplate () {
+		return $("#weather-widget-tmpl").html();
+	}
+
+	function prepareDataListItem (listItem) {
+		return {
+			day: getDayByTimestamp(listItem.dt * 1000),
+			minTemp: listItem.temp.min,
+			maxTemp: listItem.temp.max
+		};
+	}
+
+	function prepareDateToRender (data) {
+		return {
+			cityName: data.city.name,
+			forecast: data.list.map(prepareDataListItem)
+		};
+	}
+
+	$.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
 		q: "Kiev",
 		units: "metric",
 		cnt: 7
 	}, function(data) {
-		var $dayList = $("[data-js='day-list']");
-		$("[data-js='city']").text(data.city.name);
-		data.list.forEach(function(forecast){
-			$("<li></li>").text(getDayByTimestamp(forecast.dt*1000) + " min: " + forecast.temp.min + ", max: " + forecast.temp.max).appendTo($dayList);
-		});
+		var template = getTemplate(),
+			dataToRender = prepareDateToRender(data),
+			widgetHTML = Mustache.render(template, dataToRender);
+		$("#weather-widget").html(widgetHTML);
 	});
 });
