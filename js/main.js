3d2ef003de9b231d771e7cc819ddc2fbeb8edd41
nilaneo <shlikhtenkoneonila@gmail.com>
2015-02-05 17:16:52 +0200

    Add icon fonts

--- js/main.js
+++ js/main.js
@@ -8,10 +8,12 @@ $(document).ready(function() {
 	}
 
 	function prepareDataListItem (listItem) {
+		console.log(listItem.weather.icon);
 		return {
 			day: getDayByTimestamp(listItem.dt * 1000),
 			minTemp: listItem.temp.min,
-			maxTemp: listItem.temp.max
+			maxTemp: listItem.temp.max,
+			icon: getIcon(listItem.weather[0].icon)
 		};
 	}
 
@@ -20,6 +22,40 @@ $(document).ready(function() {
 			cityName: data.city.name,
 			forecast: data.list.map(prepareDataListItem)
 		};
+	}
+
+	function getIcon (iconName) {
+		switch (iconName) {
+			case "01d":
+			case "01n":
+			    return 'B';
+			case "02d":
+			case "02n":
+				return 'A';
+			case "03d":
+			case "03n":
+				return 'N';
+			case "04d":
+			case "04n":
+				return 'Y';
+			case "09d":
+			case "09n":
+				return 'R';
+			case "10d":
+			case "10n":
+				return 'Q';
+			case "11d":
+			case "11n":
+				return '0';
+			case "13d":
+			case "13n":
+				return 'W';
+			case "50d":
+			case "50n":
+				return 'M';
+			default:
+				return 'B';
+		}
 	}
 
 	$.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
